c6803456aebb420b3fb55ba3907b558599a02f37
青岛吴彦祖 <iecaser@163.com>
2017-07-20 17:37:44 +0800

    first commit

---
+++ datagen.py
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+'''
+
+@author: Soofo
+
+@license: (C) Copyright 2017-2018, IECAS Limited.
+
+@contact: iecaser@163.com
+
+@software: pycharm
+
+@file: datagen.py
+
+@time: 2017/7/20 12:15
+
+@desc:
+
+'''
+
+
